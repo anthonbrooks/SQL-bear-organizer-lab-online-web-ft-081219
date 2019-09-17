34c1a6f32b3eef2c37a73483f331ba8628ab169a
Anthony Prescott-Brooks <anthonybrooks226@gmail.com>
2019-09-17 17:06:00 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -2,6 +2,6 @@ INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Ch
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy', 1, 'male', 'brown', 'trouble-maker', 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 6, 'female', 'black', 'peaceful', 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown', 4, 'male', 'black', 'wise', 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa', 3, 'female', 'brown', '');
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa', 3, 'female', 'brown', 'short-tempered', 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch');
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Wendy');
