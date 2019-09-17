07080fd4db25941260f64f0adf5cda5746330ea4
Anthony Prescott-Brooks <anthonybrooks226@gmail.com>
2019-09-17 17:00:50 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,2 +1,2 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('');
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 3, 'Male', 'Brown', 'Huggable', 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('');
